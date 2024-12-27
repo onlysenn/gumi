9a22e8ea6197b5faf7193e04fa8e04ad7fabc913
onlysenn <sxmplywinter@gmail.com>
2024-12-27 15:48:37 +0800

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["✩ admiring the stars"];
+const statusMessages = ["𐔌ㅤ한번에 뒤엎을 actionㅤ♫"];
 const statusTypes = ['idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
